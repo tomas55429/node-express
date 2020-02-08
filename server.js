20c59463ab64a9b14506754aae5231797e5bae09
Tomas <takasawka@yahoo.com>
2020-02-08 11:35:34 -0600

    Express server part 2

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 //do this to indicate it is express file
 
+const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 //the "app" will retrun express server application
+app.use(morgan('dev'));
+//morgan is a server request logs middleware
+
+
+app.use(express.static(__dirname + '/public'));
+//the line above is what is needed to use express to serve public folders.
+//express will automatically serve index.html if a request is sent without the host name
+//the __dirname variable refers to the absolute path of the current directory of the file. 
+
 
 
 //use method can take callback function which is a middle ware function
@@ -13,7 +23,6 @@ const app = express();
 //res = is the response object
 //next = is a function
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
