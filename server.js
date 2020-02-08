fe61abeebd8ee4818af34164a765cd19dd1a2297
Tomas <takasawka@yahoo.com>
2020-02-08 12:57:19 -0600

    part 1 on express router

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 //do this to indicate it is express file
-
 const morgan = require('morgan');
+//morgan middleware used for server request logs
+const bodyParser = require('body-parser');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -9,6 +11,61 @@ const app = express();
 //the "app" will retrun express server application
 app.use(morgan('dev'));
 //morgan is a server request logs middleware
+app.use(bodyParser.json());
+
+
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain'); //a plain text will be sent back on response body
+    next(); //pass control of the application method to the next relevant application method if 
+    //request is method is a get it will go there if it is a post it will jump to post method. 
+            
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+
+//posting a data to the server
+app.post('/campsites', (req, res) => {
+    res.end(`will add the campsite: ${req.body.name} wth description: ${req.body.description}`);
+});
+
+//Updating a data on the server
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campstes');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+})
+
+
+//get method and save as campsiteId
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+
+//posting a data of a particular item on the server not supported
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+//Updating data of a particular item on the server
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+})
+
 
 
 app.use(express.static(__dirname + '/public'));
